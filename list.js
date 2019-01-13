b01f68bfa7725545cb522c35e61dd864427814b1
Khanh Nd <khanhsbv@live.com>
2019-01-14 00:22:51 +0700

    Add 0.4.25_Odyssey_v3.2.3

---
+++ list.js
@@ -0,0 +1,12 @@
+var tronSoljsonSources = [
+  "0.4.25_Odyssey_v3.2.3.js"
+];
+var tronSoljsonReleases = {
+  "0.4.25_Odyssey_v3.2.3": "0.4.25_Odyssey_v3.2.3.js",
+};
+
+if (typeof(module) !== 'undefined')
+  module.exports = {
+    'allVersions': tronSoljsonSources 
+    'releases': tronSoljsonReleases 
+  };
