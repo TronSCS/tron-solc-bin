2af70dd2ef7ed808c2f748c27e9024c7462083a4
Khanh Nd <khanhsbv@live.com>
2019-03-27 21:02:58 +0700

    Add tronbox complier

--- list.js
+++ list.js
@@ -1,8 +1,15 @@
 var tronSoljsonSources = [
-  "0.4.25_Odyssey_v3.2.3.js"
+  "0.4.25_Odyssey_v3.2.3.js",
+  "soljson_v1.js",
+  "soljson_v2.js",
+  "soljson_v3.js"
 ];
 var tronSoljsonReleases = {
   "0.4.25_Odyssey_v3.2.3": "0.4.25_Odyssey_v3.2.3.js",
+  "Tronbox SolcjsV1": "soljson_v1.js",
+  "Tronbox SolcjsV2": "soljson_v2.js",
+  "Tronbox SolcjsV3": "soljson_v3.js",
+
 };
 
 if (typeof(module) !== 'undefined')
